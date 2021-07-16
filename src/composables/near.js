c5bd419600a5159c93edf7be44c855799fef4928
hiba <hiba.mashfij@gmail.com>
2021-07-16 22:54:05 +0300

    added error handling

--- src/composables/near.js
+++ src/composables/near.js
@@ -12,29 +12,36 @@ import {
 export const useMemes = () => {
   // memes starts as an empty array
   const memes = ref([]);
+  const err = ref(null);
 
   onMounted(async () => {
     // when the component first mounts it get the memes from the blockchain
-    const memeIds = await getMemes();
 
-    memes.value = (
-      await Promise.all(
-        memeIds.map(async (id) => {
-          const info = await getMeme(id);
-          const comments = await getMemeComments(id);
+    try {
+      const memeIds = await getMemes();
 
-          return {
-            id,
-            info,
-            comments,
-            // The coming data has a 9gag link, so to display the 9gag image in the website we are splitting the 9gag link to retrieve the meme's ID. We can display the image by adding the ID to the following url: http://img-9gag-fun.9cache.com/photo/ID_460s.jpg
-            image: `https://img-9gag-fun.9cache.com/photo/${
-              info.data.split("https://9gag.com/gag/")[1]
-            }_460s.jpg`,
-          };
-        })
-      )
-    ).reverse();
+      memes.value = (
+        await Promise.all(
+          memeIds.map(async (id) => {
+            const info = await getMeme(id);
+            const comments = await getMemeComments(id);
+
+            return {
+              id,
+              info,
+              comments,
+              // The coming data has a 9gag link, so to display the 9gag image in the website we are splitting the 9gag link to retrieve the meme's ID. We can display the image by adding the ID to the following url: http://img-9gag-fun.9cache.com/photo/ID_460s.jpg
+              image: `https://img-9gag-fun.9cache.com/photo/${
+                info.data.split("https://9gag.com/gag/")[1]
+              }_460s.jpg`,
+            };
+          })
+        )
+      ).reverse();
+    } catch (e) {
+      err.value = e;
+      console.log(err.value);
+    }
   });
 
   // create a function that allows adding a message to the blockchain
